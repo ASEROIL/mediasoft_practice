4628189723534eb8873ccc58431810d2136be310
Niko <57560132+ASEROIL@users.noreply.github.com>
2020-07-16 14:05:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,19 @@
 import telebot
 import requests
+import sqlite3
+
 from telebot import types
-global step
-step = 0
+
 bot = telebot.TeleBot('992948925:AAHH7-hvNZNQZr4rYHgsRX6MfcMCeIgwtjw')
-
-def pogoda1(msg, chat_id):
-    global step
-    s_city = msg
-    city_id = 0
-    appid = "130569a52163982e4e170c25f28de658"
-    res = requests.get("http://api.openweathermap.org/data/2.5/find",
-        params={'q': s_city, 'type': 'like', 'units': 'metric', 'APPID': appid})
-    global data
-    data = res.json()
-    if (data['cod'] == '400' or len(data['list']) == 0):
-        bot.send_message(chat_id, "Извините, но информации по такому городу нет",
-            parse_mode='html')
-    elif (len(data['list']) == 1):
-        bot.send_message(chat_id, 'Сейчас ' + str(data['list'][0]['main']['temp']) + ' градусов',
-            parse_mode='html')
-    else:
-        cities = ["{} ({})".format(d['name'], d['sys']['country'])
-            for d in data['list']]
-        global i
-        i=0
-        bot.send_message(chat_id, "Я нашла следующие города:",
-            parse_mode='html')
-        while i<len(cities):
-            bot.send_message(chat_id, str(i+1)+ ": " + str(cities[i]),
-                parse_mode='html')
-            i+=1
-        bot.send_message(chat_id, "Какой город выбрать?",
-            parse_mode='html')
-        step = 2
-
-def pogoda2(msg, chat_id):
-    try:
-        global i
-        global data
-        p=int(msg)-1
-        if (p>-1 and p<i+1):
-            bot.send_message(chat_id, 'Сейчас ' + str(data['list'][p]['main']['temp']) + ' градусов',
-                parse_mode='html')
-        else:
-            bot.send_message(chat_id, "Ой, такого номера в списке нет",
-                parse_mode='html')
-    except Exception:
-        bot.send_message(chat_id, "Ой, такого номера в списке нет",
-            parse_mode='html')
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE id = ?",[message.chat.id])
+    if (not(cursor.fetchall())):
+        cursor.execute("INSERT INTO users VALUES(?,?)", [message.chat.id,'Москва'])
+    conn.commit()
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = types.KeyboardButton("Погода")
     item2 = types.KeyboardButton("Закончить")
@@ -59,30 +21,98 @@ def welcome(message):
     bot.send_message(message.chat.id, "Привет, <b>{0.first_name}</b>!\nЯ - <b>{1.first_name}</b>, ваша ассистентка.".format(message.from_user, bot.get_me()),
         parse_mode='html', reply_markup=markup)
 
-@bot.message_handler(commands=['help'])
-def send_help(message):
-    bot.send_message(message.chat.id, 'Я умею показывать погоду\nНапишите сообщение "погода" и выберите город, если совпадений несколько, отправьте номер интересующего места\nДля отмены запроса нажмите "закончить"\nВсегда рада вам помочь!',
-        parse_mode='html')
-
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    global step
     if (message.text == "Закончить"):
-        bot.send_message(message.chat.id, "Хорошо",
+        msg = bot.send_message(message.chat.id, "Хорошо",
                 parse_mode='html')
-        step = 0
-    elif (message.text == "Погода"):
-        bot.send_message(message.chat.id, "Какой город вас интересует?",
+    elif (message.text == "/help"):
+        bot.send_message(message.chat.id, 'Я умею показывать погоду\n\nНапишите сообщение "погода" и выберите город, если совпадений несколько, отправьте номер интересующего места\n\nДля отмены запроса нажмите "закончить"\n\nВсегда рада вам помочь!',
         parse_mode='html')
-        step=1
-    elif (step == 1):
-        chat_id = message.chat.id
-        pogoda1(message.text, chat_id)
-    elif (step == 2):
-        chat_id = message.chat.id
-        pogoda2(message.text, chat_id)
+    elif (message.text == "Погода"):
+        msg = bot.send_message(message.chat.id, "Какой город вас интересует?",
+        parse_mode='html')
+        bot.register_next_step_handler(msg,pogoda1)
     else:
         bot.send_message(message.chat.id, "Я вас не поняла",
             parse_mode='html')
+
+@bot.message_handler(content_types=['text'])
+def pogoda1(message):
+    if (message.text == "Закончить"):
+        msg = bot.send_message(message.chat.id, "Хорошо",
+                parse_mode='html')
+        bot.register_next_step_handler(msg,send_text)
+        return
+    elif (message.text == "/help"):
+        msg = bot.send_message(message.chat.id, 'Я умею показывать погоду\n\nНапишите сообщение "погода" и выберите город, если совпадений несколько, отправьте номер интересующего места\n\nДля отмены запроса нажмите "закончить"\n\nВсегда рада вам помочь!',
+            parse_mode='html')
+        bot.register_next_step_handler(msg,pogoda1)
+        return
+    s_city = message.text
+    appid = "130569a52163982e4e170c25f28de658"
+    res = requests.get("http://api.openweathermap.org/data/2.5/find",
+        params={'q': s_city, 'type': 'like', 'units': 'metric', 'APPID': appid})
+    data = res.json()
+    if (data['cod'] == '400' or len(data['list']) == 0):
+        msg = bot.send_message(message.chat.id, "Извините, но информации по такому городу нет",
+            parse_mode='html')
+        bot.register_next_step_handler(msg,pogoda1)
+    elif (len(data['list']) == 1):
+        msg = bot.send_message(message.chat.id, 'Сейчас ' + str(data['list'][0]['main']['temp']) + ' градусов',
+            parse_mode='html')
+        bot.register_next_step_handler(msg,pogoda1)
+    else:
+        conn = sqlite3.connect("users.db")
+        cursor = conn.cursor()
+        cursor.execute("UPDATE users SET city1 = ? where id = ?", [s_city,message.chat.id])
+        conn.commit()
+        cities = ["{} ({})".format(d['name'], d['sys']['country'])
+            for d in data['list']]
+        i=0
+        bot.send_message(message.chat.id, "Я нашла следующие города:",
+            parse_mode='html')
+        while i<len(cities):
+            bot.send_message(message.chat.id, str(i+1)+ ": " + str(cities[i]),
+                parse_mode='html')
+            i+=1
+        msg = bot.send_message(message.chat.id, "Какой город выбрать?",
+            parse_mode='html')
+        bot.register_next_step_handler(msg,pogoda2)
+
+@bot.message_handler(content_types=['text'])
+def pogoda2(message):
+    if (message.text == "Закончить"):
+        msg = bot.send_message(message.chat.id, "Хорошо",
+                parse_mode='html')
+        bot.register_next_step_handler(msg,send_text)
+        return
+    elif (message.text == "/help"):
+        msg = bot.send_message(message.chat.id, 'Я умею показывать погоду\n\nНапишите сообщение "погода" и выберите город, если совпадений несколько, отправьте номер интересующего места\n\nДля отмены запроса нажмите "закончить"\n\nВсегда рада вам помочь!',
+            parse_mode='html')
+        bot.register_next_step_handler(msg,pogoda2)
+        return
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT city1 FROM users WHERE id = ?",[message.chat.id])
+    s_city = cursor.fetchall()[0][0]
+    appid = "130569a52163982e4e170c25f28de658"
+    res = requests.get("http://api.openweathermap.org/data/2.5/find",
+        params={'q': s_city, 'type': 'like', 'units': 'metric', 'APPID': appid})
+    data = res.json()
+    try:
+        p=int(message.text)-1
+        if (p>-1 and len(data['list'])):
+            bot.send_message(message.chat.id, 'Сейчас ' + str(data['list'][p]['main']['temp']) + ' градусов',
+                parse_mode='html')     
+        else:
+            msg = bot.send_message(message.chat.id, "Ой, такого номера в списке нет",
+                parse_mode='html')            
+    except Exception:
+        msg = bot.send_message(message.chat.id, "Ой, такого номера в списке нет",
+                parse_mode='html')
+    msg = bot.send_message(message.chat.id, "Какой город еще выбрать?",
+        parse_mode='html')
+    bot.register_next_step_handler(msg,pogoda2)
     
 bot.polling()
